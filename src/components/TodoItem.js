b7867851d18d2ce1c9ea2fdec59a48c745fae283
Luane Souza <luanesouza@Luanes-MacBook-Pro.local>
2019-02-09 16:10:57 -0500

    markComplete function created

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
   getStyle = () => {
-    if(this.props.todo.completed) {
-      return {
-        textDecoration: 'line-through'
-      }
-    } else {
-      return {
-        textDecoration: 'none'
-      }
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
     }
   }
+
   render() {
     return (
       <div style={this.getStyle()}>
-        <p>{ this.props.todo.title }</p>
+        <p>
+          <input type="checkbox" onChange={this.props.markComplete}/> {' '}
+          { this.props.todo.title }</p>
       </div>
     )
   }
