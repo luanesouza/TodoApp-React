f56b55756fa941bd75875f11e61c7bb709eed2f0
Luane Souza <luanesouza@Luanes-MacBook-Pro.local>
2019-02-09 19:23:33 -0500

    delete function done

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -18,7 +18,9 @@ export class TodoItem extends Component {
       <div style={this.getStyle()}>
         <p>
           <input type="checkbox" onChange={this.props.markComplete.bind (this, id)}/> {' '}
-          { title }</p>
+          { title }
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
+        </p>
       </div>
     )
   }
@@ -27,6 +29,15 @@ export class TodoItem extends Component {
 //PropTypes
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+  background: '#ff0000',
+  color: '#fff',
+  padding: '5px 9px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
 }
 
 export default TodoItem
